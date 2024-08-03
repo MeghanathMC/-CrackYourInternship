e8708f7ae5c05411c15c7fc7b0db2d3773bd1347
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2024-08-03 11:16:54 +0530

    Time: 2 ms (75.40%) | Memory: 53.4 MB (7.33%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,9 +1,8 @@
 class Solution {
+    public int el;
     public int majorityElement(int[] arr) {
 
-        int n=arr.length;
-        
-
+      /*  int n=arr.length;
         for(int i=0;i<n;i++){
             int cnt=0;
             for(int j=0;j<n;j++){
@@ -16,7 +15,58 @@ class Solution {
                 return arr[i];
             }
         }
-        return -1;
+        return -1;*/
+
+
+        // HashMap<Integer,Integer> mp=new HashMap<>();
+
+        // int n=nums.length;
+
+        // for(int i=0;i<n;i++){
+        //     int value=mp.getOrDefault(nums[i],0);
+        //     mp.put(nums[i],value+1);
+        // }
+
+        // //now to count the occurences
+
+        // for(Map.Entry<Integer,Integer> it : mp.entrySet()){
+        //     if(it.getValue() > (n/2)){
+        //         return it.getKey();
+        //     }
+
+        // }
+
+        // return -1;
+        int n=arr.length;
+        int cnt=0;
         
+
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                el=arr[i];
+                cnt++;
+            }
+            else if(arr[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+
+
+        int cnt1=0;
+
+        for(int j=0;j<n;j++){
+            if(arr[j]==el){
+                cnt1++;
+            }
+        
+
+        if(cnt1>(n/2)){
+            return arr[j]; 
+        }
+        }
+        return -1;
     }
 }
