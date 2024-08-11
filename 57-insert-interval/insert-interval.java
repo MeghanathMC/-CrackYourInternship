00d88ec72da25a1aa0d85f758fc632a20416aa30
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2024-08-11 13:14:35 +0530

    Time: 1 ms (98.75%) | Memory: 44.7 MB (74.38%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -10,8 +10,8 @@ class Solution {
         }
 
         while(i<n && intervals[i][0] <=newInterval[1]){
-            newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
-            newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+            newInterval[0]=Integer.min(newInterval[0],intervals[i][0]);
+            newInterval[1]=Integer.max(newInterval[1],intervals[i][1]);
             i++;
         }
 
